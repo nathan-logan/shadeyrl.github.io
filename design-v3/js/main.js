582ef9c16e40e35b391e88678680089c85069bd4
shadeyrl <37327997+shadeyrl@users.noreply.github.com>
2018-06-08 21:07:39 +1000

    scrolltop

--- design-v3/js/main.js
+++ design-v3/js/main.js
@@ -1,5 +1,14 @@
 $(".arrow").on("click", function(){
     $('html, body').animate({
-        scrollTop: $(".tl-container").offset().top
-    }, 1000);
+        scrollTop: $("#arrow-anchor").offset().top
+    }, 1250);
 });
+
+$(document).scroll(function(){
+        if($(window).scrollTop() === 0) {
+            $(".nav").removeClass("scrolled");
+        }
+        else {
+            $(".nav").addClass("scrolled");
+        }
+    })
